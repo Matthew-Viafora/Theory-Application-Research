5a097d03e09c50d6d13f3ab8e66ff462045f909a
Matthew Viafora <55547270+Matthew-Viafora@users.noreply.github.com>
2021-06-07 15:47:55 -0400

    Updated Script

--- parsing.py
+++ parsing.py
@@ -1,19 +1,44 @@
 # Theory, Application, and Gender in research
 # Matthew Viafora, Jhanvi Ganesh, Glendon Chin, Harrison Chachko
-# 6/4/21
+# Last updated: 6/7/21
 import xml.etree.ElementTree as ET
 import pandas as pd
 
 # Create rows and columns for csv
-cols = ["author", "title", "venue", "year", "url"]
+cols = ["year","first-author","last-author","journal/conference","title","number-of-authors", "paper-url", "DBLP-url"]
 rows = []
 
 # Parsing the XML file
 xmlparse = ET.parse('/Users/mattviafora/Library/Mobile Documents/com~apple~CloudDocs/GitHub/Python Data Science/Theory-Application-Research/34_test.xml')
+
 root = xmlparse.getroot()
 for i in root.iter("info"):
+    # get attributes
     title = getattr(i.find("title"), 'text', "nan")
-    rows.append({ "title" : title})
+    year = getattr(i.find("year"), 'text', "nan")
+    conference = getattr(i.find("venue"), "text", "nan")
+    paper_url = getattr(i.find("ee"), "text", "nan")
+    DBLP_url = getattr(i.find("url"), "text", "nan")
+
+    # get author count and first/last authors
+    author_count = 0
+    last_child = None
+    for child in i.find("authors"):
+        if author_count == 0:
+            first_author = child.text
+        author_count+=1
+        last_child = child
+    last_author = last_child.text
+    
+    # Append all attributes to csv
+    rows.append({"year" : year, 
+                 "number-of-authors" : author_count, 
+                 "title" : title, 
+                 "first-author" : first_author, 
+                 "last-author" : last_author,
+                 "journal/conference" : conference,
+                 "paper-url" : paper_url,
+                 "DBLP-url" : DBLP_url}) 
 
 df = pd.DataFrame(rows, columns=cols)
 
