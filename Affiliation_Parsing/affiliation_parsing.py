daffea77e08f831ee8c1f7c7a0d8868dd01b6061
Matthew Viafora <55547270+Matthew-Viafora@users.noreply.github.com>
2021-06-21 16:44:34 -0400

    Add files via upload

---
+++ Affiliation_Parsing/affiliation_parsing.py
@@ -0,0 +1,90 @@
+from csv import DictReader
+import requests
+from bs4 import BeautifulSoup
+import os
+import PyPDF2
+import urllib.request
+import io
+import pandas as pd
+
+cols = ['first-author', 'first-author-affiliation', 'last-author', 'last-author-affiliation', 'title']
+rows = []
+
+nuerips_url = 'https://neurips.cc/Conferences/2020/Schedule?type=Poster'
+
+html = requests.get(nuerips_url)
+soup = BeautifulSoup(html.content, 'html.parser')
+
+results = soup.find(id='main')
+
+tags = results.find_all('div')
+for tag in tags:
+        try:
+
+            paper_title_target = tag.find('div', class_ = 'maincardBody')
+            paper_title = paper_title_target.text
+
+
+            onclick = tag['onclick']
+
+            paper_tag_url = 'https://neurips.cc/Conferences/2020/Schedule?showEvent=' + onclick[11:16]
+            html_2 = requests.get(paper_tag_url)
+            soup_2 = BeautifulSoup(html_2.content, 'html.parser')
+            results_2 = soup_2.find('div',attrs={'class':'col-xs-12 col-sm-9'})
+            tags_2 = results_2.find_all('button')
+            author_tag_first = None
+            author_tag_last = None
+            temp = None
+            count = 0
+            for tag_2 in tags_2:
+                if count == 0:
+                    author_tag_first = tag_2['onclick']
+                    count +=1
+                temp = tag_2['onclick']
+            author_tag_last = temp
+
+            first_author_url = 'https://neurips.cc/Conferences/2020/Schedule?showSpeaker=' + author_tag_first[13:24]
+            last_author_url = 'https://neurips.cc/Conferences/2020/Schedule?showSpeaker=' + author_tag_last[13:24]
+
+        
+            html_request_first = requests.get(first_author_url)
+            html_request_last = requests.get(last_author_url)
+            soup_first = BeautifulSoup(html_request_first.content, 'html.parser')
+            soup_last = BeautifulSoup(html_request_last.content, 'html.parser')
+            results_first = soup_first.find(id='whaat')
+            results_last = soup_last.find(id='whaat')
+            
+            affiliation_first_target = results_first.find('h4')
+            affiliation_last_target = results_last.find('h4')
+            name_first_target = results_first.find('h3')
+            name_last_target = results_last.find('h3')
+
+            name_first = name_first_target.text
+            name_last = name_last_target.text
+            affiliation_first = affiliation_first_target.text
+            affiliation_last = affiliation_last_target.text
+
+            # print("______________________________________________________________________________")
+            # print("Paper: " + paper_title)
+            # print()
+            # print("First author: " + name_first)
+            # print("First author affiliation: " + affiliation_first)
+            # print()
+            # print("Last author: " + name_last)
+            # print("Last author affiliation: " + affiliation_last)
+            # print("______________________________________________________________________________")
+
+
+            rows.append({"first-author" : name_first,
+                         "first-author-affiliation" : affiliation_first,
+                         "last-author" : name_last,
+                         "last-author-affiliation" : affiliation_last,
+                         "title" : paper_title})
+
+        except:
+            continue
+
+df = pd.DataFrame(rows, columns=cols)
+
+       
+
