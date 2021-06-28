006cc9dfc8d63a7c6deb7ddf4422a687a3e75992
Harrison Chachko <hchachko@gmail.com>
2021-06-28 17:03:25 -0400

    Matt put your camera on

--- Affiliation_Parsing/CSV_Merging.py
+++ Affiliation_Parsing/CSV_Merging.py
@@ -12,18 +12,28 @@ with open(path+'/Affiliation_Parsing2010output.csv', "r") as AffiliationCSV:
         main_csv_reader = csv.reader(MainCSV, delimiter=',')
         
         #Parse through rows of both files
+        
         for aRows in affiliation_csv_reader:
-            print(aRows[5])                  #Error, works here but doesn't when placed within nested loop
+            count = 0
+            aTitle = aRows[5]
+            aFirstAffiliation = aRows[2]
+            aLastAffiliation = aRows[4]
+            # print(aTitle)                  #Error, works here but doesn't when placed within nested loop
             for mRows in main_csv_reader:
-                
+                count +=1
+                print (count)
+                print(aTitle)
                 #If the titles of each paper match, let the affiliations be updated for that paper
-                if aRows[5] == mRows[7]:
+                if aTitle == mRows[7]:
                     print("Main file's first affilation before: " + mRows[3])
                     print("Main file's second affilation before: " + mRows[5])
 
                     #Update Affiliation data
-                    mRows[3] = aRows[2]
-                    mRows[5] = aRows[4]
+                    mRows[3] = aFirstAffiliation
+                    mRows[5] = aLastAffiliation
 
                     print("Main file's first affilation after: " + mRows[3])
                     print("Main file's second affilation after: " + mRows[5])
+                    continue
+                
+            
